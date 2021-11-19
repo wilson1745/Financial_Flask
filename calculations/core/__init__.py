a69d2160446c164b7299caaab2fc474dc56a60ec
wilsonlo0130 <wilsonlo@cybersoft4u.com>
2021-11-19 10:42:26 +0800

    Decide Log path when identifying the OS system

--- calculations/core/__init__.py
+++ calculations/core/__init__.py
@@ -1,3 +1,4 @@
+import platform
 import multiprocessing
 from datetime import datetime
 from pathlib import Path
@@ -5,18 +6,47 @@ from pathlib import Path
 from calculations.common.constants.constants import INIT
 from calculations.core import log
 
-""" Top level than other modules in core package """
-PATH = (str(Path(Path(__file__).resolve().parents[1])) + r"\files\logs")
-# Check if this directory is exist
-Path(PATH).mkdir(parents=True, exist_ok=True)
+system_os = platform.system()
+try:
+    SLASH = ""
+    if system_os == 'Windows':
+        SLASH = "\\"
+    elif system_os == 'Darwin':
+        SLASH = "\\"
+    elif system_os == 'Linux':
+        SLASH = "/"
+    else:
+        raise Exception(f"Unknown system: {system_os}")
 
-LOG = log.Logger((PATH + r"\%s.log") % datetime.now().strftime('%Y_%m_%d'), level='debug').logger
-LOG.info(f"Initialize Log.Logger: {LOG}")
+    """ Top level than other modules in core package """
+    # r"\files\logs" or r"/files/logs"
+    PATH = (str(Path(Path(__file__).resolve().parents[1])) + SLASH + "files" + SLASH + "logs")
+    # Check if this directory is exist
+    Path(PATH).mkdir(parents=True, exist_ok=True)
 
-""" For parallel process (Do not use all my processing power) """
-# CPU_THREAD = multiprocessing.cpu_count()
-CPU_THREAD = multiprocessing.cpu_count() - 1
-LOG.info(f"Initialize CPU_THREAD: {CPU_THREAD} ")
+    # r"\%s.log" or r"/%s.log"
+    LOG = log.Logger((PATH + SLASH + r"%s.log") % datetime.now().strftime('%Y_%m_%d'), level='debug').logger
+    LOG.info(f"Initialize Log.Logger: {LOG}")
+
+    """ For parallel process (Do not use all my processing power) """
+    # CPU_THREAD = multiprocessing.cpu_count()
+    CPU_THREAD = multiprocessing.cpu_count() - 1
+    LOG.info(f"Initialize CPU_THREAD: {CPU_THREAD} ")
+except Exception as e:
+    raise Exception(f"Unknown system: {system_os}")
+
+# """ Top level than other modules in core package """
+# PATH = (str(Path(Path(__file__).resolve().parents[1])) + r"\files\logs")
+# # Check if this directory is exist
+# Path(PATH).mkdir(parents=True, exist_ok=True)
+#
+# LOG = log.Logger((PATH + r"\%s.log") % datetime.now().strftime('%Y_%m_%d'), level='debug').logger
+# LOG.info(f"Initialize Log.Logger: {LOG}")
+#
+# """ For parallel process (Do not use all my processing power) """
+# # CPU_THREAD = multiprocessing.cpu_count()
+# CPU_THREAD = multiprocessing.cpu_count() - 1
+# LOG.info(f"Initialize CPU_THREAD: {CPU_THREAD} ")
 
 if __name__ == '__main__':
     LOG.info(f"Calculations Core {INIT} 作為主程序啟動")
